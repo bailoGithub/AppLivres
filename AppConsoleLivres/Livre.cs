9a6d6e716c7141fbfcdfcb3b2c0a1afe4492edd1
Oumarou Bailo Bah <oumaroubailo.bah@monboreal.ca>
2024-10-17 18:41:07 -0400

    la classe livre a ete ajoutee avec succes

---
+++ AppConsoleLivres/Livre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+
+        public int NombrePages { get; set; }
+        public string Titre {  get; set; }
+        public string Auteur { get; set; }
+
+        public Livre(string titre , string auteur , int nombrePages) { 
+        
+        Titre = titre;
+        Auteur = auteur;
+        NombrePages = nombrePages;
+        
+        }
+        public void AfficherInfoDuLivre() 
+        {
+            Console.WriteLine($"le titre du live est : {Titre} , et lauteur de ce livre est :{Auteur} , pour un nombre de page de {NombrePages}");
+            
+        
+        }
+
+    }
+}
